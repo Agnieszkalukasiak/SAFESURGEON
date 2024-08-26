60221aa4de13b5b7146bc79844ab964cc285ee8f
Agnieszka Lukasiak <lukasiak@me.com>
2024-08-26 19:21:37 +0000

    chnaged the model

--- SafeFaceIntro/models.py
+++ SafeFaceIntro/models.py
@@ -2,17 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
-
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="verification"
-)
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
+    STATUS = ((0, "Draft"), (1, "Published"))
 
-def __str__(self):
-    return self.title
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="verification")
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    clinic_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    id_document = models.FileField(upload_to='id_documents/', null=True, blank=True)
+    diploma = models.FileField(upload_to='diplomas/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.author.username}'s Post - {self.clinic_name}"
+
+class Education(models.Model):
+    post = models.ForeignKey(Post, related_name='education', on_delete=models.CASCADE)
+    school = models.CharField(max_length=200)
+    program = models.CharField(max_length=200)
+    years = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.school} - {self.program}"
