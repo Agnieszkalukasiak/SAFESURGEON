acc245d8d95a35844117560fdf22c2c31e283c34
Agnieszka Lukasiak <lukasiak@me.com>
2024-08-26 11:22:27 +0000

    fix bug dijango project setup

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://u04dpx4dcly:LMZGoqSquSwO@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/shell_giver_flip_797988")
+os.environ.setdefault("SECRET_KEY", "<your_choice_of_secret_key>")
