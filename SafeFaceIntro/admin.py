058bebbb45de1ea895f189afd4a81fe176056719
Agnieszka Lukasiak <lukasiak@me.com>
2024-08-28 14:23:48 +0000

    fix admin bug

--- SafeFaceIntro/admin.py
+++ SafeFaceIntro/admin.py
@@ -3,15 +3,22 @@ from .models import Beauticians, Education
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Beauticians)
-class PostAdmin(SummernoteModelAdmin):
+class BeauticiansAdmin(SummernoteModelAdmin):
+    list_display = ('get_name', 'clinic', 'city', 'country', 'verification_status')
+    search_fields = ['user__first_name', 'user__last_name', 'clinic', 'city', 'country']
+    list_filter = ('verification_status', 'city', 'country')
 
-    list_display = ('name', 'clinic','city','country')
-    search_fields = ['name']
-    list_filter = ('verification_status',)
-    prepopulated_fields = {'clinic': ('name',)}
-    summernote_fields = ('education',)
+    def get_name(self, obj):
+        return obj.user.get_full_name()
+    get_name.short_description = 'Name'
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('beautician', 'school', 'program')
+    search_fields = ['beautician__user__first_name', 'beautician__user__last_name', 'school', 'program']
+    list_filter = ('school',)
+    summernote_fields = ('description',) 
 
 # Register your models here.
 
-admin.site.register(Education)
 
