14754ea9ddc13cf057efae5cc3120f1f27480e4f
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-01 20:48:33 +0000

    update setting.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'cloudinary',
-    'SafeFaceIntro',
+    'SafeSurgeon',
     
 ]
 
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SafeFaceIntro', 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SafeSurgeon', 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
