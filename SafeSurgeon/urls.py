5aa66bfcdf98d5cf03afc8a25a672f762a572c00
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-06 19:19:24 +0000

    add html surgeon_profile,add url for editing

--- SafeSurgeon/urls.py
+++ SafeSurgeon/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.verify, name='verify'), 
     path('get-verified/', views.get_verified, name='get_verified'),
     path('submit-surgeon-form/', views.submit_surgeon_form, name='submit_surgeon_form'),
+    path('surgeon-profile/', views.surgeon_profile, name='surgeon_profile'),
+     path('edit-profile/', views.edit_profile, name='edit_profile'),
 ]
