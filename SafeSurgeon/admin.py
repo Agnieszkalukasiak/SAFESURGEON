42c07288f1bf9ba4a696d64165047ceb9aa92be6
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-12 15:29:22 +0000

    remove surgeonresgister admin, and the manual reg

--- SafeSurgeon/admin.py
+++ SafeSurgeon/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Verification, Country, City, Clinic, Surgeon, Education
+from .models import Verification, Country, City, Surgeon, Education
 from django_summernote.admin import SummernoteModelAdmin
 from django.contrib.auth.models import User
 
@@ -58,17 +58,17 @@ class SurgeonAdmin(SummernoteModelAdmin):
         return obj.city.country.name if obj.city and obj.city.country else None
     get_country.short_description = 'Country'
      
-    #def user_first_name(self, obj):
-    #    return obj.user.first_name  # Accessing the first_name from the related User model
-    #user_first_name.short_description = 'First Name'
+    def user_first_name(self, obj):
+       return obj.user.first_name  # Accessing the first_name from the related User model
+    user_first_name.short_description = 'First Name'
 
-    #def user_last_name(self, obj):
-    #    return obj.user.last_name  # Accessing the last_name from the related User model
-    #user_last_name.short_description = 'Last Name'
+    def user_last_name(self, obj):
+        return obj.user.last_name  # Accessing the last_name from the related User model
+    user_last_name.short_description = 'Last Name'
 
-    #def user_email(self, obj):
-    #    return obj.user.email  # Accessing the email from the related User model
-    #user_email.short_description = 'Email'
+    def user_email(self, obj):
+        return obj.user.email  # Accessing the email from the related User model
+    user_email.short_description = 'Email'
 
     # Education admin
     @admin.register(Education)
@@ -79,17 +79,5 @@ class SurgeonAdmin(SummernoteModelAdmin):
 
 
     
-    
 
    
-
-
-
-#Education Admin
-@admin.register(Education)
-class EducationAdmin(admin.ModelAdmin):
-    list_display = ('surgeon', 'institution', 'institution_country', 'program',  'start_date', 'end_date', 'certificate')
-    list_filter = ('institution', 'program','institution_country')
-    search_fields = ['user__first_name', 'user__last_name', 'institution', 'program','country__name']
-
-admin.site.register(Surgeon, SurgeonAdmin)
