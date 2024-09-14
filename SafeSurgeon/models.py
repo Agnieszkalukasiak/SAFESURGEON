a483b22e648655e52fce4912fbc141d5e74d1797
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-14 10:31:14 +0000

    update models with default models

--- SafeSurgeon/models.py
+++ SafeSurgeon/models.py
@@ -47,65 +47,21 @@ class Clinic(models.Model):
         return f"{self.name}"
 
     @classmethod
-    def default_clinic(cls):
-        get_default_clinic, created = cls.objects.get_or_create(
+    def get_default_clinic(cls):
+        default_clinic,_ = cls.objects.get_or_create(
             name="Default Clinic",
             defaults={'name': "Default Clinic"}
         )
         return default_clinic
 
-#default user for development
-def default_user_and_surgeon():
-    #create or get default user
-    default_user, created = User.objects.get_or_create(
-        username='default_user', 
-        defaults={
-        'first_name': 'Default',
-        'last_name': 'User',
-        'email': 'default@example.com',
-        'password':'defaultpassword' 
-    })
-
-    
-
-    #create or get the dafult surgon liked to the default user
-    default_surgeon,created = Surgeon.objects.get_or_create(
-        user=default_user,
-        defaults = {
-            'profile_picture':'default_profile_pic.jpg',
-            'clinic':'default clinic',
-            'city':default_city,
-            'country': default_country,
-            'verification_status': Verification.PENDING,
-            'id_document':'default_id_document.jpg',
-            'slug':slugify(f"default-surgeon-{default_user.id}")
-        }
-    )
-            #default education records
-    Education.objects.get_or_create(
-        surgeon=default_surgeon,
-        institution="default university",
-        program= "default program",
-        institution_country ="institution country",
-        start_date= '2001-02-02',
-        end_date= '2004-02-02',
-    )
-
-    return default_surgeon
-
-    #usage Uncomment for testing
-#default_surgeon=default_user_and_surgeon()
-#   print (default_surgeon.user_display())
-#else:
-#    print ("default_surgeon not found.")
 
 
 class Surgeon(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="surgeon_verification")
     profile_picture = CloudinaryField('profile picture', folder='profilePicture', default='default_profile_pic', null=True, blank=True)
-    clinic = models.ForeignKey(Clinic, on_delete=models.SET_NULL, null=True, related_name="surgeons")
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="surgeon")
-    country = models. ForeignKey(Country, on_delete=models.CASCADE, related_name="surgeon")
+    clinic = models.ForeignKey(Clinic, on_delete=models.SET_DEFAULT,default=Clinic.get_default_clinic, related_name="surgeons")
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="surgeons")
+    country = models. ForeignKey(Country, on_delete=models.CASCADE, related_name="surgeons")
     verification_status = models.CharField(
         max_length=9, 
         choices=Verification.choices, 
@@ -117,7 +73,7 @@ class Surgeon(models.Model):
         
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(f"{self.user_first_name}-{self.user_last_name}")
+            self.slug = slugify(f"{self.user.first_name}-{self.user.last_name}")
     
         if not self.city or not self.country:
             default_country, _ = Country.objects.get_or_create(name='Default Country')
@@ -150,6 +106,32 @@ class Surgeon(models.Model):
             'education': [f"{edu.institution} - {edu.program}" for edu in self.education.all()]
         }
 
+    @classmethod
+    def default_user_and_surgeon(cls):
+            #create or get default user
+        default_user, created = User.objects.get_or_create(
+        username='default_user', 
+        defaults={
+            'first_name': 'Default',
+            'last_name': 'User',
+            'email': 'default@example.com',
+            'password':'defaultpassword' 
+        }
+    )
+
+    #create or get the dafult surgon liked to the default user
+        default_surgeon,_ = cls.objects.get_or_create(
+            user=default_user,
+            defaults = {
+                'profile_picture':'default_profile_pic.jpg',
+                'clinic':'default clinic',
+                'city':default_city,
+                'country': default_country,
+                'verification_status': Verification.PENDING,
+                'id_document':'default_id_document.jpg',
+                'slug':slugify(f"default-surgeon-{default_user.id}")
+            }
+        )
 
 class Education(models.Model):
     surgeon  = models.ForeignKey(Surgeon, related_name='education', on_delete=models.CASCADE)
@@ -172,17 +154,21 @@ class Education(models.Model):
     def __str__(self):
         return f"{self.institution} - {self.program} - {self.start_date} to {self.end_date}"
 
-# class SurgeonVerification(models.Model):
-#    user_first_name = models.CharField(max_length=100)
-#    user_last_name = models.CharField(max_length=100)
- #   profile_picture = CloudinaryField('profile picture', folder='profilePicture', default='default_profile_pic', null=True, blank=True)
- #   clinic = models.CharField(max_length=100)
- #   city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="cities")            #this was just a charfield, can go back
- #   country = models. ForeignKey(Country, on_delete=models.CASCADE, related_name="countries") #this was just a charfield, can go back
- #   verification_status = models.CharField(
- #       max_length=9, 
- #       choices=Verification.choices, 
- #       default=Verification.PENDING
- #   )
- #   def __str__(self):
- #       return f"{self.first_name} {self.last_name} - {self.clinic}"
+    @classmethod        
+    #default education records
+    def create_default_education(cls, surgeon):
+       default_education, _ = cls.objects.get_or_create(
+            surgeon=surgeon,
+            defaults={
+                'institution': "default university",
+                'program': "default program",
+                'institution_country': "institution country",
+                'start_date': '2001-02-02',
+                'end_date': '2004-02-02',
+            }
+        )
+
+    Education.create_default_education(default_surgeon)
+
+    return default_surgeon
+
