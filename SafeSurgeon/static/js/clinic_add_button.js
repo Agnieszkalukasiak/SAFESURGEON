36bfaa0ee4219f0a7b4cf44acf5c98845bc49676
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-19 13:19:06 +0000

    add filtering citites

--- SafeSurgeon/static/js/clinic_add_button.js
+++ SafeSurgeon/static/js/clinic_add_button.js
@@ -16,6 +16,7 @@
             let formCount = parseInt(managementForm.value);
             console.log("Current form count:", formCount);
     
+    
             if (clinicFormsContainer.children.length > 0) {
                 const lastForm = clinicFormsContainer.children[clinicFormsContainer.children.length - 1];
                 const newForm = lastForm.cloneNode(true);
@@ -48,55 +49,26 @@
     }
 });
 
-/*
-
+/*city select */
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM loaded from inline script");
-    const addClinicButton = document.getElementById('add-clinic');
-    const clinicFormsContainer = document.getElementById('clinic-forms');
-    const managementForm = document.querySelector('input[name$=TOTAL_FORMS]');
+    const countrySelect = document.querySelector('select[name="country"]');
+    const citySelect = document.querySelector('select[name="city"]');
+    const cityUrl = window.cityUrl;
 
-    console.log("Add Clinic button:", addClinicButton);
-    console.log("Clinic Forms Container:", clinicFormsContainer);
-    console.log("Management Form:", managementForm);
-
-    if (addClinicButton && clinicFormsContainer && managementForm) {
-        addClinicButton.addEventListener('click', function() {
-            console.log("Add Clinic button clicked");
-            alert("Add Clinic button clicked!");
-
-            let formCount = parseInt(managementForm.value);
-            console.log("Current form count:", formCount);
-
-            if (clinicFormsContainer.children.length > 0) {
-                const lastForm = clinicFormsContainer.children[clinicFormsContainer.children.length - 1];
-                const newForm = lastForm.cloneNode(true);
-                console.log("New form cloned");
-
-                newForm.innerHTML = newForm.innerHTML.replace(/clinic_set-\d+/g, `clinic_set-${formCount}`);
-                newForm.innerHTML = newForm.innerHTML.replace(/-\d+-/g, `-${formCount}-`);
-                console.log("Form index updated");
-
-                newForm.querySelectorAll('input:not([type=hidden]), select').forEach(input => {
-                    input.value = '';
+    countrySelect.addEventListener('change', function() {
+        const countryId = this.value;
+        if (countryId) {
+            fetch(`${cityUrl}${countryId}`)
+                .then(response => response.json())
+                .then(data => {
+                    citySelect.innerHTML = '<option value="">---------</option>';
+                    data.cities.forEach(city => {
+                        const option = new Option(city.name, city.id);
+                        citySelect.add(option);
+                    });
                 });
-                console.log("Input values cleared");
-
-                formCount++;
-                managementForm.value = formCount;
-                console.log("Total forms updated to:", managementForm.value);
-
-                clinicFormsContainer.appendChild(newForm);
-                console.log("New form appended");
-            } else {
-                console.error("No existing clinic forms to clone");
-            }
-        });
-    } else {
-        console.error("Required elements not found");
-        console.log("Add Clinic button:", addClinicButton);
-        console.log("Clinic Forms Container:", clinicFormsContainer);
-        console.log("Management Form:", managementForm);
-    }
+        } else {
+            citySelect.innerHTML = '<option value="">---------</option>';
+        }
+    });
 });
-*/
