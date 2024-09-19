b5962df3fac441af1ddd6d103febb3f5f02d1b9d
Agnieszka Lukasiak <lukasiak@me.com>
2024-09-19 12:48:24 +0000

    add city filter in get_verfieid, in html, js

--- SafeSurgeon/static/js/Ajax_get_verified.js
+++ SafeSurgeon/static/js/Ajax_get_verified.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log("Script loaded: [Ajax_get_verified].js");
     const countrySelect = document.querySelector('select[name="country"]');
     const citySelect = document.querySelector('select[name="city"]');
-    const cityUrl = "{% url 'get_cities' 0 %}".slice(0, -1);
+    const cityUrl = window.city.url;
 
     countrySelect.addEventListener('change', function() {
         const countryId = this.value;
