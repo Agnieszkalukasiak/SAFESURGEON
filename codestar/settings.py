24ec789d9709b97d97f69a4a4e1eab67d3bd9918
Agnieszka Lukasiak <lukasiak@me.com>
2024-10-01 08:34:26 +0000

    change debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-agnieszkalu-safesurgeon-jnqo6orcz3z.ws.codeinstitute-ide.net',
  'safesurgeon-f2f78c12cdcc.herokuapp.com',
