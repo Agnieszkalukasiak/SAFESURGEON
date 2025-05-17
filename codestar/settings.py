ea172241f2aec8fb3dac22500ea62067891e342f
Agnieszkalukasiak <lukasiak@me.com>
2025-05-17 16:19:21 +0000

    chnage database

--- codestar/settings.py
+++ codestar/settings.py
@@ -139,6 +139,13 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 #DATABASES = {
  #   'default': {
   #      'ENGINE': 'django.db.backends.sqlite3',
@@ -147,15 +154,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
 }
-
-#DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
-#}
 
 
 # Default primary key field type
